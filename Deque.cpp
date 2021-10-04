a5485d61b9853e3f45f23f925edcd1a59de1cb2a
Raed56 <rseraj@usf.edu>
2021-10-04 06:39:19 -0400

    Deque implmentation mostly done

--- Deque.cpp
+++ Deque.cpp
@@ -6,8 +6,51 @@ Deque::Deque() { //constructor
     size = 0; //initialize size 
 }
 
-std::string Deque::back() {
 
-    
+int Deque::size() const{
+    return size;
+} 
+
+
+std::string Deque::top(){
+
+} 
+std::string Deque::end(){
+
+}  
+
+const Elem& Deque::getFront() const throw (DequeEmpty){
+
+    return list.front();
 }
+
+const Elem& Deque::getBack() const throw (DequeEmpty){
+    return list.back();
+}
+
+void Deque::insertFront(const Elem& e){
+    list.addFront(e);
+    size++;
+}
+
+void Deque::insertBack(const Elem& e){
+    list.addBack(e);
+    size++;
+}
+
+void Deque::removeFront() throw (DequeEmpty){
+    if (empty()){
+        throw DequeEmpty("This is an empty deque.");
+    }
+    list.removeFront();
+    size--;
+}
+
+void Deque::removeBack() throw (DequeEmpty){
+    if (empty()){
+        throw DequeEmpty("This is an empty deque.");
+    }
+    list.removeBack();
+    size--;
+}  
 
--- Deque.hpp
+++ Deque.hpp
@@ -8,18 +8,23 @@ class Deque {
         Deque(); //constructor
         int size() const; //gets the size of the deque 
         bool empty() {return list.empty();}; //returns if the deque is empty 
+
+
+        //Question: Do we need top() and end() if we have front() and back()
+
         std::string top(); //retruns the first value  
         std::string end(); //returns the end value  
-        const Elem& front() const;
-        const Elem& back() const;
+
+        const Elem& getFront() const throw (DequeEmpty);        //return the front value/element
+        const Elem& getBack() const throw (DequeEmpty);         //return the end value/element
 
         void insertFront(const Elem& e); 
         void insertBack(const Elem& e);
-        void removeFront();
-        void removeBack();  
+        void removeFront() throw (DequeEmpty);
+        void removeBack() throw (DequeEmpty);  
 
     private: 
         int size; 
         DLinkedList list; 
-        friend class DequeList;
+        friend class DequeList; //Question: Do we need to friend any other class? and what is the DequeList class?
 };
